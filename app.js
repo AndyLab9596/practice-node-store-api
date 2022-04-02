292ab3c5fd6717298c3c2f54a429b35c25a26fd8
andy <vy.chung@hdwebsoft.co>
2022-04-02 10:05:37 +0700

    init proj

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+const connectDB = require('./db/connect');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`)
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
