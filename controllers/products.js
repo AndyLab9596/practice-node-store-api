fe62b903aaffef41879d4cb3f960c18bd00c3acb
andy <vy.chung@hdwebsoft.co>
2022-04-02 10:42:06 +0700

    populate data

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,19 @@
+const Product = require('../models/product');
+
 const getAllProducts = async (req, res) => {
-    res.send('Get all Products')
+    const { featured } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    let result = Product.find(queryObject)
+    // Chaining method
+
+    //
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = { getAllProducts }
